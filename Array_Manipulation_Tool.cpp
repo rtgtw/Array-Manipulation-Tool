ec0a4cd2a519c1f140e509dff63fec0c2fad17f8
Remi Olateru <remiolateru@gmail.com>
2024-01-15 14:01:09 -0500

    Created Switch statement

--- Array_Manipulation_Tool.cpp
+++ Array_Manipulation_Tool.cpp
@@ -210,7 +210,7 @@ int setElement(struct Array* arr, int index, int set) {
 		return 0;
 	}
 
-	std::cout << "Invalid Index. Try again";
+	std::cout << "Invalid Index. Try again" << '\n';
 	//returns -1 to signify failure
 	return -1;
 };
@@ -284,24 +284,79 @@ int maxElement(struct Array arr) {
 
 
 
+//Sum of elements in array
+//-------------------------------------------------------------------------------------//
+//Find the sum of the full array
+//Pass the array as a parameter
+int sumOfElementsWithinArray(struct Array arr) {
+
+	//Create a local variable total to represent a count for the total sum
+	int total = 0;
+
+	//create a for loop to iterate through each element within the array
+	for (int i = 0; i < arr.length; i++) {
+
+		//with each iteration, add the element with total, and store it within total
+		total += arr.A[i];
+
+	};
+
+	//return total
+	return total;
+
+};
+
+
+
+
+//Reverse elements in array
+//-------------------------------------------------------------------------------------//
+//pass in array pointer into parameter since we will be changing the value of the array
+//This can be either int or void, its void if you dont want to return a value
+//it can be int if you want to return 0 for successful or 1 for unsucessful, i will use int
+int reverseArraySwap(struct Array* arr) {
+
+
+	//We want to create two index points, one at the beginning of the array, and one at the end
+	// then we want to swap each element until the two indexes cross, thats when we know we are finished
+	for (int i = 0, j = arr->length - 1; i < j; i++, j--) {
+
+		//We can use the swap element functions previously created to swap the elements by address
+		//We must use pointers since we are changing the values and want this change to
+		//be true even after the function ends
+		swapElements(&arr->A[i], &arr->A[j]);
+
+
+		//return 0 for success
+		std::cout << "Successfully reversed array" << '\n';
+		return 0;
+	};
+
+	//return 1 for unsuccessful
+	std::cout << "Unsuccessful reverse" << '\n';
+	return 1;
+};
 
 
 
 
 
 
+//Display elements in array
+//-------------------------------------------------------------------------------------//
 
+void display(struct Array arr) {
+	int i;
 
+	std::cout << '\n' << '\n' << '\n';
+	std::cout << "Elements are: " << '\n';
+	for (i = 0; i < arr.length; i++) {
+		std::cout << arr.A[i] << '\n';
+	}
 
+	std::cout << '\n' << '\n';
 
-
-
-
-
-
-
-
-
+};
 
 
 
@@ -320,128 +375,125 @@ int main() {
 	//Create an Array object
 	struct Array array1;
 
-
-
-
-	//Create variables
-	int index;
-	int element;
-	int element2;
-
-
 	//Ask user to input Array Size
 	std::cout << "Enter the size you want your array to be: ";
 	std::cin >> array1.size;
-	
+
 	//Initialize the value of Array Object's length to 0
 	array1.length = 0;
 
 	//Dynamically create an object array based off user input
 	array1.A = new int[array1.size];
 
+	//Create variables
+	int index;
+	int element;
+	int element2;
+	int choices;
 
+	do {
 
+		std::cout << "[] Menu []" << '\n';
+		std::cout << "1. Insert " << '\n';
+		std::cout << "2. Append " << '\n';
+		std::cout << "3. Delete " << '\n';
+		std::cout << "4. Get Index " << '\n';
+		std::cout << "5. Swap " << '\n';
+		std::cout << "6. Set Element " << '\n';
+		std::cout << "7. Min " << '\n';
+		std::cout << "8. Max " << '\n';
+		std::cout << "9. Sum " << '\n';
+		std::cout << "10. Reverse " << '\n';
+		std::cout << "11. Display " << '\n';
+		std::cout << "12. Exit " << '\n';
+		std::cout << '\n' << " Enter your choice: ";
+		std::cin >> choices;
 
+		//Create switch statement
+		switch (choices) {
+		case 1:
+			//Ask user for index and element to insert into array
+			std::cout << "Enter an Index and Element to insert into array: ";
+			std::cin >> index;
+			std::cin >> element;
 
-	//Ask user for index and element to insert into array
-	std::cout << "Enter an Index and Element to insert into array: ";
-	std::cin >> index;
-	std::cin >> element;
-	
+			//Insert Element
+			insertElement(&array1, index, element);
+			break;
+		case 2:
+			//Ask user what value they would like to append to the existing array
+			std::cout << "Enter a Value that you would like to append to the array: ";
+			std::cin >> element;
 
-	//Insert Element
-	insertElement(&array1, index, element);
+			//Append Element
+			append(&array1, element);
 
+			break;
+		case 3:
+			//Ask user at which index would they like to delete a value?
+			std::cout << "Enter the Index at which you would like to delete a value: ";
+			std::cin >> index;
 
+			//Delete Element
+			deleteElement(&array1, index);
 
+			break;
+		case 4:
+			//Ask user to enter an element to get its index
+			std::cout << "Enter a value within the array to obtain its index: ";
+			std::cin >> element;
 
+			//show the index
+			std::cout << getIndex(array1, element) << '\n' << '\n';
+			break;
+		case 5:
+			//Ask user to enter two elements to swap
+		//We will use the getIndex function, to find both indexes of the elements
+		//In order to pass elements in the swap functions parameters
+		//SwapElements function takes in the memory addresses in order to swap
+			std::cout << "Enter two elements to swap within the array: ";
+			std::cin >> element;
+			std::cin >> element2;
+			swapElements(&array1.A[getIndex(array1, element)], &array1.A[getIndex(array1, element2)]);
 
+			break;
+		case 6:
 
+			//Ask the user to enter an Index and Element to set value
+			std::cout << "Set an element by entering the Index and new Element Value: ";
+			std::cin >> index;
+			std::cin >> element;
+			setElement(&array1, index, element);
 
-	//Ask user what value they would like to append to the existing array
-	std::cout << "Enter a Value that you would like to append to the array: ";
-	std::cin >> element;
+			break;
+		case 7:
+			//Find the min element
+			std::cout << "Min Element is: " << minElement(array1) << '\n' << '\n';
 
-	//Append Element
-	append(&array1, element);
+			break;
+		case 8:
 
+			//Find the max element
+			std::cout << "Max Element is: " << maxElement(array1) << '\n' << '\n';
+			break;
+		case 9:
+			//Find the sum of elements
+			std::cout << "Sum of all elements in array is: " << sumOfElementsWithinArray(array1) << '\n' << '\n';
+			break;
+		case 10:
+			//Reverse array
+			reverseArraySwap(&array1);
+			break;
+		case 11:
+			//Display Elements
+			display(array1);
+			break;
+		case 12:
+			break;
 
+		}
+	} while (choices < 12);
 
-
-
-	//Ask user at which index would they like to delete a value?
-	std::cout << "Enter the Index at which you would like to delete a value: ";
-	std::cin >> index;
-
-	//Delete Element
-	deleteElement(&array1, index);
-
-
-
-
-	//Ask user to enter an element to get its index
-	std::cout << "Enter a value within the array to obtain its index: ";
-	std::cin >> element;
-
-	//show the index
-	std::cout << getIndex(array1, element) << '\n' << '\n';
-
-
-
-
-
-	//Ask user to enter two elements to swap
-	//We will use the getIndex function, to find both indexes of the elements
-	//In order to pass elements in the swap functions parameters
-	//SwapElements function takes in the memory addresses in order to swap
-	std::cout << "Enter two elements to swap within the array: ";
-	std::cin >> element;
-	std::cin >> element2;
-	swapElements(&array1.A[getIndex(array1, element)], &array1.A[getIndex(array1, element2)]);
-
-	
-
-	
-
-
-	//Ask the user to enter an Index and Element to set value
-	std::cout << "Set an element by entering the Index and new Element Value: ";
-	std::cin >> index;
-	std::cin >> element;
-	setElement(&array1, index, element);
-
-
-
-
-
-
-	//Find the min element
-	std::cout << "Min Element is: " << minElement(array1) << '\n' << '\n';
-
-
-
-	//Find the max element
-	std::cout << "Max Element is: " << maxElement(array1) << '\n' << '\n';
-
-
-
-
-	//Display Element
-	for (int i = 0; i < array1.length; i++) {
-		std::cout << array1.A[i] << '\n';
-	};
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 	
 	return 0; 
